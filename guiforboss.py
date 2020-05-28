d88f1be2fb4f49a4821fcf6ee0bad1dc2150f64a
Shiho <shihohohohohohoho@gmail.com>
2020-05-29 02:25:10 +0800

    e

--- guiforboss.py
+++ guiforboss.py
@@ -47,22 +47,22 @@ class Window(QMainWindow):
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle("Subscribe to Raishin Aot")
         self.setStyleSheet("background-color: grey;")
-        width,height = 600,400
+        width,height = 800,600
         x_pos,self.button_y = 0,20
         logo = Label(10,self.button_y,278,278,"osr2mp4_logo.png","osr2mp4_logo.png","",self)
         image_list = ["select_osr.png","select_osr_hover.png","select_osu.png","select_osu_hover.png","select_skin.png","select_skin_hover.png","output.png","output_hover.png"]
         file_type = [".osr",".osu","folder","output"]
         file_typeCounter = 0
-        scale = int(height/469)
-        self.button_width = 257 * scale
-        self.button_height = 46 * scale
+        scale = height/469
+        self.button_width = int(257 * scale)
+        self.button_height = int(46 * scale)
 
-        self.start_width = 178 * scale
-        self.start_height = 77 * scale
+        self.start_width = int(178 * scale)
+        self.start_height = int(77 * scale)
         self.main_buttons = []
         for x in range(0,len(image_list) - 1,2):
             button = Label(width-self.button_width,self.button_y,self.button_width,self.button_height,image_list[x],image_list[x+1],file_type[file_typeCounter],self)
-            self.button_y+=50
+            self.button_y+=70
             file_typeCounter+=1
             self.main_buttons.append(button)
 
@@ -80,17 +80,16 @@ class Window(QMainWindow):
     def resizeEvent(self, event):
         ypos = 20
         
-        scale = int(self.height()/469)
-        self.button_width = 257 * scale
-        self.button_height = 46 * scale
+        scale = self.height()/469
+        self.button_width = int(257 * scale)
+        self.button_height = int(46 * scale)
         
         progressbar_width,progressbar_height = self.width() - 20,30
-        print(self.width())
-        print(progressbar_width)
+
         progressbar_x,progressbar_y = 10,self.height()-40
         for x in self.main_buttons:
             x.setGeometry(self.width()-self.button_width,ypos,self.button_width,self.button_height)
-            ypos+=50
+            ypos+=70
 
         self.progressBar.setGeometry(progressbar_x,progressbar_y,progressbar_width,32)
         self.start.setGeometry(progressbar_x + progressbar_width - self.start_width,self.height()-40-self.start_height,self.start_width,self.start_height)       
