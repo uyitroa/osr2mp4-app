8d3d69a702d74dfa1285671640790501401c1241
Shiho <shihohohohohohoho@gmail.com>
2020-06-18 00:31:36 +0800

    ok

--- ScrollArea.py
+++ ScrollArea.py
@@ -2,6 +2,9 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
+from QLabel import Titles, Small_Titles
+from Textbox import Big_Textbox, Small_Textbox
+from Slider import Slider
 class Scroll_Class():
 	def __init__(self,parent):
 		super().__init__()
@@ -46,59 +49,58 @@ class Scroll_Class():
 			background: transparent;
 		}
 				  """
+
+
 		scrollArea.verticalScrollBar().setStyleSheet(styleSheet)
 		scrollArea.verticalScrollBar().setValue(0)
 		render_ = QtWidgets.QLabel("Render Options")
 		render_.setStyleSheet("""font: bold 24px;color:white;""")
 
+		#Vertical
 		separator = QtWidgets.QLabel()
 		separator_img = QtGui.QPixmap('res/Separator.png')
 		separator_img = separator_img.scaled(500, 10, QtCore.Qt.KeepAspectRatio)
 		separator.setPixmap(separator_img)
 
-		paths = ["osu! path:", "Output path:"]
-		osu_pathLabel = QtWidgets.QLabel(paths[0])
-		osu_pathLabel.setStyleSheet("font: bold 12px;color:white;")
-		osu_pathInput = QtWidgets.QLineEdit()
-		osu_pathInput.setFixedWidth(300)
+		osu_pathLabel = Titles("osu! path")
+		osu_pathInput = Big_Textbox()
 
-		output_pathLabel = QtWidgets.QLabel(paths[1])
-		output_pathLabel.setStyleSheet("font: bold 12px;color:white;")
-		output_pathInput = QtWidgets.QLineEdit()
-		output_pathInput.setFixedWidth(300)
+		output_pathLabel = Titles("Output path:")
+		output_pathInput = Big_Textbox()
 
-		width_label = QtWidgets.QLabel("Width:")
-		width_label.setStyleSheet("font: bold 12px;color:white;")
-		width_Input = QtWidgets.QLineEdit()
+		duration_slider = Slider() 
+		duration_start = Small_Titles("Start time:")
+		#Horizontal
+		width_label = Small_Titles("Width:")
+		width_input = Small_Textbox()
 
-		fps_label = QtWidgets.QLabel("FPS:")
-		fps_label.setStyleSheet("font: bold 12px;color:white;")
-		fps_Input = QtWidgets.QLineEdit()
-		fps_Input.setFixedWidth(300)
+		fps_label = Small_Titles("Width:")
+		fps_input = Small_Textbox()
 
-		codec_label = QtWidgets.QLabel("Video codec:")
-		codec_label.setStyleSheet("font: bold 12px;color:white;")
-		codec_Input = QtWidgets.QLineEdit()
+		codec_label = Small_Titles("Video codec:")
+		codec_input = Small_Textbox()
+
+		process_label = Small_Titles("Process:")
+		process_input = Small_Textbox()
 
 		vertical_widgets = []
 		horizontal_widgets = []
 
-		vertical_widgets.extend((render_, separator, osu_pathLabel, osu_pathInput, output_pathLabel, output_pathInput))
-		horizontal_widgets.extend((width_label, width_Input, fps_label, fps_Input, codec_label, codec_Input))
-
+		vertical_widgets.extend((render_, separator, osu_pathLabel, osu_pathInput, output_pathLabel, output_pathInput, duration_start, duration_slider, process_label, process_input))
+		horizontal_widgets.extend((width_label, width_input, fps_label, fps_input, codec_label, codec_input))
 		for x in range(len(vertical_widgets)):
+			if x == 1:
+				continue
 			self.gridLayout.addWidget(vertical_widgets[x], x, 0)
+		self.gridLayout.addWidget(separator,1, 0, 1, 3)
 
-		for x in range(2,len(horizontal_widgets),1):
-			self.gridLayout.addWidget(horizontal_widgets[x], x, 1)
+		for x in range(len(horizontal_widgets)):
+			self.gridLayout.addWidget(horizontal_widgets[x], x+2, 1)
 
-
+		self.gridLayout.setRowStretch(100,1)
 		#scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+		print("settings")
 
-
-		for x in range(2,200,1):
-			self.gridLayout.addWidget(QtWidgets.QLabel("Test"), 2, x)
-			print("brother")
 
 
 
