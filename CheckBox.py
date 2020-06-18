ec58db054c8da1a3bfb638573bde542420f7848f
Shiho <shihohohohohohoho@gmail.com>
2020-06-18 23:13:52 +0800

    idek

---
+++ CheckBox.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QCheckBox
+
+
+class CheckBox(QCheckBox):
+	def __init__(self, title, jsondata=None):
+		super().__init__()
+		self.default_width = 20
+		self.default_height = 20
+		self.setStyleSheet("""
+QCheckBox::indicator {
+    width: 20px;
+    height: 20px;
+}
+QCheckBox::indicator:unchecked {
+    image: url(res/Uncheck_HD.png);
+}
+QCheckBox::indicator:checked {
+    image: url(res/Check_HD.png);
+}
+			""")
+		self.setText(title)
+		self.setStyleSheet("font: bold 14px;color:white")
+
+		# Do your setStylesheet here
