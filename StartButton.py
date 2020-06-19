e776634b00ca177eac5fff8d70c75d316950d053
shintaroDesu <snkraishin87@gmail.com>
2020-06-19 20:35:25 +0200

    update settings when click start button

--- StartButton.py
+++ StartButton.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 from Parents import Button
-from config_data import current_config
+from config_data import current_config, current_settings
 
 
 class StartButton(Button):
@@ -29,5 +29,9 @@ class StartButton(Button):
 		with open('config.json', 'w+') as f:
 			json.dump(current_config, f, indent=4)
 			f.close()
-		print(current_config)
+		with open('settings.json', 'w+') as f:
+			json.dump(current_settings, f, indent=4)
+			f.close()
+
+
 		self.proc = subprocess.Popen(["python3", "run_osu.py"], shell=False)
