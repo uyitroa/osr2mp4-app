8c43a4ff6599d12ef5ef3dafa4c329b7a5f61809
shintaroDesu <snkraishin87@gmail.com>
2020-06-20 21:21:45 +0200

    real run osu

--- run_osu.py
+++ run_osu.py
@@ -3,39 +3,7 @@ import time
 
 
 def run():
-	for x in range(101):
-	  b = open("progress.txt", "w")
-	  b.write(str(x))
-	  b.close()
-	  time.sleep(0.1)
-	'''fprogress = open("progress.txt", "w")
-				fprogress.seek(0)
-				fprogress.write("0")
-				fprogress.truncate()
-			
-				converter = Osr2mp4(filedata="config.json", filesettings="settings.json")
-				converter.startall()
-			
-				curprogress = 0
-				while curprogress < 95:
-					curprogress = converter.getprogress()
-					
-					a = False
-					for i in converter.writers:
-						if i.is_alive():
-							a = True
-					if not a:
-						curprogress = 100
-			
-					fprogress.seek(0)
-					fprogress.write(str(curprogress))
-					fprogress.truncate()
-					print(curprogress)
-					time.sleep(2)
-				converter.joinall()'''
-
-
-	'''fprogress = open("progress.txt", "w")
+	fprogress = open("progress.txt", "w")
 	fprogress.write("0")
 	fprogress.close()
 
@@ -43,22 +11,31 @@ def run():
 	converter.startall()
 
 	curprogress = 0
-	while curprogress < 95:
+	while curprogress < 99:
 		curprogress = converter.getprogress()
 
 		a = False
+
+		if converter.writers is None:
+			break
+
 		for i in converter.writers:
 			if i.is_alive():
 				a = True
 		if not a:
-			curprogress = 100
+			curprogress = 99
 
 		fprogress = open("progress.txt", "w")
 		fprogress.write(str(curprogress))
 		fprogress.close()
 		print(curprogress)
-		time.sleep(0.2)
-	converter.joinall()'''
+		time.sleep(1)
+	converter.joinall()
+
+	fprogress = open("progress.txt", "w")
+	fprogress.write("100")
+	fprogress.close()
+
 
 if __name__ == "__main__":
 	run()
